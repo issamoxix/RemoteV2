28330923254e4ce03fb9a8096a39d623781b6551
Issam Haidaoui <Issam.Haidaoui@hqrevenue.com>
2022-07-17 14:30:18 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  let videoId = "E8BJadt0iXc";
+  const ytbRef = useRef();
+  useEffect(() => {
+    console.log("refx", ytbRef)
+
+  }, [ytbRef.current])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <YouTube videoId={videoId} opts={opts} ref={ytbRef} onPause={() => {
+        ytbRef.current.internalPlayer.playVideo()
+        console.log("refx State changed",ytbRef.current.state)
+      }} />
     </div>
   );
 }

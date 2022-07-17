34de455ffe8b695c2bdf4fd02eef8f4176f9f92b
Issam Haidaoui <Issam.Haidaoui@hqrevenue.com>
2022-07-17 15:36:38 +0200

    added video url input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import YouTube from 'react-youtube';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+function getVideoId(url) {
+  let urlParsed = new URL(url);
+  let value;
+  if (url.includes(".be")) {
+    let roi = urlParsed.pathname;
+    value = roi.substr(1)
+    return value
+  }
+  value = urlParsed.searchParams.get('v')
+  return value
+}
 
 function App() {
+  const [input, setinput] = useState()
+  const [videoId, setvideoId] = useState("E8BJadt0iXc")
   const opts = {
     height: '390',
     width: '640',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
+      autoplay: 0,
     },
   };
-  let videoId = "E8BJadt0iXc";
   const ytbRef = useRef();
   useEffect(() => {
     console.log("refx", ytbRef)
@@ -19,10 +32,18 @@ function App() {
   }, [ytbRef.current])
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Youtube Player ;) </h1>
+      <div className='VideoIdInputs' >
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          setvideoId(getVideoId(input))
+        }}>
+          <input type="text" placeholder='Youtube Video Link ' onChange={(e) => setinput(e.target.value)} />
+          <input type="submit" value="Play" onClick={() => setvideoId(getVideoId(input))} />
+        </form>
+      </div>
       <YouTube videoId={videoId} opts={opts} ref={ytbRef} onPause={() => {
         ytbRef.current.internalPlayer.playVideo()
-        console.log("refx State changed",ytbRef.current.state)
       }} />
     </div>
   );
